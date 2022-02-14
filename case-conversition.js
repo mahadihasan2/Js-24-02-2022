fd50ac15328aa9aac9d88bacc57c4fc7cf183264
mahadihasan2 <mahadihasan125643@gmail.com>
2022-02-14 13:04:15 +0600

    case-conversition

---
+++ case-conversition.js
@@ -0,0 +1,12 @@
+const strings = "Ami tumar Moner Manus moner e akjon";
+const search = "valo";
+// console.log(strings.toLocaleLowerCase());
+
+if (strings.toLocaleLowerCase == search.toLocaleLowerCase) {
+  console.log("This is a True");
+}
+const upperCase = "tumar Mone Joto shukh ace sob amake daw";
+const upperCase2 = "pakhi jai chole jai . dil ki doya hoi na";
+if (upperCase.toUpperCase == upperCase2.toUpperCase) {
+  console.log("Halimone Jabo");
+}
